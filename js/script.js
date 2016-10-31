cc767138f5c5f9d8f21f8cf67b3845268dad1d9b
Eric Snell <eric_snell21@yahoo.com>
2016-10-31 19:28:53 -0400

    background now flat color, added font awesome icons and google font

--- js/script.js
+++ js/script.js
@@ -2,12 +2,10 @@ $(document).ready(() => {
   // VARIABLES
   let quote;
   let author;
-  let image;
 
   // FUNCTIONS
   const getQuote = () => {
     // AJAX GET request to quote generating API
-    console.log('getQuote function called');
     $.ajax({
       type: 'GET',
       url: 'http://quotes.rest/qod.json?category=inspire'
@@ -15,19 +13,18 @@ $(document).ready(() => {
     .done((result) => {
       quote = result.contents.quotes[0].quote;
       author = result.contents.quotes[0].author;
-      image = result.contents.quotes[0].background;
       $(".quote-text").text(quote);
       $(".author-text").text("-" + author);
-      $("body").css("background-image", "url('" + image + "')");
     })
     .fail((error) => {
-      console.log(error);
+      console.log("Error: ", error);
     });
   };
 
   const tweetQuote = () => {
     const tweetText = $(".quote-text").text();
-    window.open("http://twitter.com/home?status=" + tweetText, "_blank");
+    const
+    window.open("http://twitter.com/home?status=" + '"' + tweetText + '"', "_blank");
   };
 
   // EVENT HANDLERS
@@ -36,7 +33,6 @@ $(document).ready(() => {
   });
 
   $(".next-button").click(() => {
-    console.log('button clicked');
     getQuote();
   });
 
