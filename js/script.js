aaca081c5a77108988422c0485f20da3e8a73297
Eric Snell <eric_snell21@yahoo.com>
2017-03-07 09:05:10 -0500

    ES6 syntax fixes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 $(document).ready(() => {
 
-  /*=== VARIABLES ===*/
-  let quote
-  let author
-
-
   /*=== FUNCTIONS ===*/
   function getQuote() {
 
@@ -23,44 +18,46 @@ $(document).ready(() => {
     .done((result) => {
       const res = JSON.parse(result)
 
+      let quote = res.quote
+      let author = res.author
+
       $('.quote-text').fadeOut(function() {
-        $(this).text(res.quote).fadeIn(1000)
+        $(this).text(quote).fadeIn(1000)
       })
 
       $('.author-text').fadeOut(function() {
-        let that = this
-        setTimeout(function() {
-          $(that).text('- ' + res.author).fadeIn(1000)
+        setTimeout(() => {
+          $(this).text('- ' + author).fadeIn(1000)
         }, 1500)
       })
-
     })
 
     // Handle's any error with request
-    .fail((error) => {
-      console.log("Error: ", error)
-    })
+    .fail(error => console.log("Error: ", error))
+
   }
+
+
 
   // Opens Twitter window to share the quote
   function tweetQuote() {
     const tweetText = $(".quote-text").text()
     const tweetMovie = $(".author-text").text()
 
-    window.open("http://twitter.com/home?status=" + '"' + tweetText + '"' + ' ' + tweetMovie, "_blank")
+    window.open(
+      `http://twitter.com/home?status="${ tweetText }" ${ tweetMovie }`, '_blank'
+    )
   }
 
 
-  /*=== EVENT HANDLERS ===*/
-  $(".tweet-button").click(() => {
-    tweetQuote()
-  })
 
-  $(".next-button").click(() => {
-    getQuote()
-  })
+  /*=== EVENT HANDLERS ===*/
+  $(".tweet-button").click(() => tweetQuote())
+
+  $(".next-button").click(() => getQuote())
 
 
   /*=== PAGE LOAD ===*/
   getQuote()
+  
 })
